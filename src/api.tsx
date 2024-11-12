8f0582e67a7b58e4ede2a024339265d66c2fcc34
Shariqzain <shariqzain06@gmail.com>
2024-11-12 20:38:59 +0530

    Update api.tsx

--- src/api.tsx
+++ src/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'http://139.84.136.164';
 // Update the axios instance configuration
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || `${BASE_URL}`,
